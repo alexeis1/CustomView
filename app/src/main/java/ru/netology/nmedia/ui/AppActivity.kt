3627361dfb3b0df4b930d0d0df464922e7b8ab95
alexeis1 <alexei-s1@yandex.ru>
2021-07-25 01:45:49 +0300

    Initial commit

---
+++ app/src/main/java/ru/netology/nmedia/ui/AppActivity.kt
@@ -0,0 +1,20 @@
+package ru.netology.nmedia.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.ActivityAppBinding
+import java.security.AccessController.getContext
+
+
+class AppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityAppBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.stats.data = listOf(0.25F, 0.25F, 0.25F, 0.25F)
+
+    }
+}
